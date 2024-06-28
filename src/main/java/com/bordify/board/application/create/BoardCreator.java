72b09cca5d7593556d050a0753666ec5ca53ca0e
JuanC <abjuancarlos.12@gmail.com>
2024-06-28 00:58:37 -0500

    feat: create service for to create a Board

---
+++ src/main/java/com/bordify/board/application/create/BoardCreator.java
@@ -0,0 +1,26 @@
+package com.bordify.board.application.create;
+
+import com.bordify.board.domain.Board;
+import com.bordify.board.domain.BoardRepository;
+import com.bordify.exceptions.ResourceNotCreatedException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardCreator {
+
+    private BoardRepository boardRepository;
+
+    /**
+     * Creates a new boardEntity and saves it in the database.
+     *
+     * @param board The boardEntity entity to be saved.
+     * @throws ResourceNotCreatedException if the boardEntity cannot be created.
+     */
+    public void createBoard(Board board) {
+        try {
+            boardRepository.save(board);
+        } catch (Exception e) {
+            throw new ResourceNotCreatedException("Error creating boardEntity");
+        }
+    }
+}
