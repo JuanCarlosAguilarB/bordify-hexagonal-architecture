f52dc9e740e644445f844592ef983e740821aa6d
JuanC <abjuancarlos.12@gmail.com>
2024-06-29 04:05:39 -0500

    feat: rename repository and create method for get all colors paginated

--- src/main/java/com/bordify/color/infrastructure/persistence/ColorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bordify.color.infrastructure.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Repository interface for accessing and managing color entities in the database.
- */
-public interface ColorRepository extends JpaRepository<Color, Integer> {
-}
