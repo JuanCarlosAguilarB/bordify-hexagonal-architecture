63015b9533c459ff7609145c5f525e2f1af2d729
JuanC <abjuancarlos.12@gmail.com>
2024-06-29 04:06:03 -0500

    feat: rename repository of color.

--- src/main/java/com/bordify/color/domain/ColorRepository.java
+++ src/main/java/com/bordify/color/domain/ColorRepository.java
@@ -1,8 +1,14 @@
 package com.bordify.color.domain;
 
+import com.bordify.shared.domain.PageResult;
+import com.bordify.shared.domain.PaginationRequest;
+
+import java.util.List;
+
 /**
- * Repository interface for accessing and managing color entities in the database.
+ * Repository interface for accessing and managing colorEntity entities in the database.
  */
 public interface ColorRepository {
 
+    public PageResult<Color> findAll(PaginationRequest  paginationRequest);
 }
