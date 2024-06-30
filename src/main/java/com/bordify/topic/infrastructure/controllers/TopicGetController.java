7e63a72ac310a2d995286b9886fcd5cacbc2ccca
JuanC <abjuancarlos.12@gmail.com>
2024-06-29 20:41:35 -0500

    feat: service to search for topics from a board is implemented

---
+++ src/main/java/com/bordify/topic/infrastructure/controllers/TopicGetController.java
@@ -0,0 +1,32 @@
+package com.bordify.topic.infrastructure.controllers;
+
+import com.bordify.services.TaskService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@AllArgsConstructor
+public class TopicGetController {
+
+    private final TaskService taskService;
+
+    /**
+     * Retrieves tasks associated with a topicEntity.
+     *
+     * @param topicId  The ID of the topicEntity.
+     * @param pageable Pagination information.
+     * @return ResponseEntity with tasks of the specified topicEntity.
+     */
+    @GetMapping("/topic/{topicId}/tasks/")
+    public ResponseEntity<?> getTaskOfTopic(@PathVariable UUID topicId, Pageable pageable) {
+        return ResponseEntity.ok(taskService.getTaskOfTopic(topicId, pageable));
+    }
+
+
+}
